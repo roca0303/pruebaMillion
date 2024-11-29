b302ea4fdaad53c4544f8097996ce66921146fec
rocaa0303 <ro_campeon@hotmail.com>
2024-11-28 19:45:00 -0500

    clean code and add more info func

--- src/components/Carrusel/Carrusel.tsx
+++ src/components/Carrusel/Carrusel.tsx
@@ -5,13 +5,15 @@ import { Tag } from 'primereact/tag';
 import ProductService  from '../Services/PropertyService';
 import Filters from '../Filters/Filters';
 import { InputText } from 'primereact/inputtext';
+import { Dialog } from 'primereact/dialog';
 
 export interface Product {
   id: string;
+  idOwner: string;
   ownerName: string;
   image: string;
   price: string;
-  propertyAddress: number;
+  propertyAddress: string;
   propertyName: string;
 }
 
@@ -23,6 +25,17 @@ export default function ResponsiveDemo() {
     minprice: string;
     maxprice: string;
   }
+
+  let emptyProduct = {
+    id: '',
+    idOwner: '',
+    ownerName: '',
+    image: '',
+    price: '',
+    propertyAddress: '',
+    propertyName: '',
+  };
+  
 
   let emptyProperty = {
     nombre: '',
@@ -40,6 +53,8 @@ export default function ResponsiveDemo() {
   });
 
   const [products, setProducts] = useState<Product[]>([]);
+  const [cardPropertyInfo, setCardPropertyInfo] = useState(false);
+  const [selectedProperty, setSelectedProperty] = useState<Product>();
 
   const responsiveOptions = [
     {
@@ -70,6 +85,28 @@ export default function ResponsiveDemo() {
 
   }, []);
 
+  const onInfoPropertyClick = (propertyId: string) => {
+
+    // find property by id
+    const propertyInfo = products.find((property) => property.id === propertyId);
+
+    if (!propertyInfo) {
+      return;
+    }
+    setSelectedProperty(propertyInfo);
+    setCardPropertyInfo(true);
+  };
+
+  const hideDialog = () => {
+    setCardPropertyInfo(false);
+  }
+
+  const productDialogFooter = (
+    <React.Fragment>
+        <Button label="Cancel" icon="pi pi-times" className="p-button-text" onClick={hideDialog} />
+    </React.Fragment>
+  );
+
   const productTemplate = (property: Product) => {
     return (
       <div className="border-1 surface-border border-round m-2 text-center py-5 px-3 ">
@@ -84,12 +121,17 @@ export default function ResponsiveDemo() {
         </div>
         <div>
           <h4 className="mb-1">{property.propertyName}</h4>
+          <h5 className="mb-1">{property.propertyAddress}</h5>
           <h6 className="mt-0 mb-3">
             {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(parseFloat(property.price))}
           </h6>
           <Tag value={property.ownerName} ></Tag>
           <div className="mt-5 flex flex-wrap gap-2 justify-content-center">
-            <Button icon="pi pi-search" className="p-button p-button-rounded" />
+          <Button 
+            icon="pi pi-search" 
+            className="p-button p-button-rounded"  
+            onClick={() => onInfoPropertyClick(property.id)} 
+          />
           </div>
         </div>
       </div>
@@ -126,12 +168,10 @@ export default function ResponsiveDemo() {
             <label htmlFor="nombre" className="htmlForTag">Name:  </label>
             <InputText id="nombre" value={product.nombre} onChange={(e) => onInputChange(e, 'nombre')} required autoFocus  />
         </div>
-
         <div className="field">
             <label htmlFor="address" className="htmlForTag">Address:  </label>
             <InputText id="address" value={product.address} onChange={(e) => onInputChange(e, 'address')} required autoFocus  />          
         </div>
-
         <div className="field">
             <label htmlFor="minprice" className="htmlForTag">Min Price: </label>
             <InputText id="minprice" value={product.minprice} onChange={(e) => onInputChange(e, 'minprice')} required autoFocus style={{ width: '100px', padding: '5px' }} />
@@ -140,20 +180,25 @@ export default function ResponsiveDemo() {
             <label htmlFor="maxprice" className="htmlForTag">Max Price: </label>
             <InputText id="maxprice" value={product.maxprice} onChange={(e) => onInputChange(e, 'maxprice')} required autoFocus  style={{ width: '100px', padding: '5px' }} />          
         </div>
-
-
         <div className="field">
           <Button label="Buscar" icon="pi pi-search" onClick={onSearchClick} />
         </div>
       </div>
 
       <div className="divcarrusel">
-
         <div className="card">
             <Carousel value={products} numScroll={1} numVisible={3} responsiveOptions={responsiveOptions} circular
             autoplayInterval={4000} itemTemplate={productTemplate} />
         </div>
-      </div>    
+      </div>
+
+
+      <Dialog visible={cardPropertyInfo} style={{ width: '450px' }} header={`Información de: ${selectedProperty?.propertyName || ''}`}  modal className="p-fluid" footer={productDialogFooter} onHide={hideDialog}>
+        <p>Address: {selectedProperty?.propertyAddress}</p>
+      </Dialog>
+
+
+
     </div>
   )
 }
